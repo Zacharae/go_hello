c2b46f3eb77082e5efa81a6e482f291449fe7bd8
Zacharae <zacharyxae@gmail.com>
2016-09-28 21:39:34 -0400

    hello world sings a song

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf("Hello, world.\n You are my sunshine.\n  You make me happy when skies are grey.\n   You'll never know dear, how much I love you\n    Please don't take my sunshine away\n     ")
 }
